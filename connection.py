41bedfd4d679f3e64a7b9ce01e257c31f98ff66c
Lakisa00 <radjenlazar@gmail.com>
2025-09-16 21:44:22 +0200

    Initial commit

---
+++ connection.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_NAME = os.getenv("DB_NAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+conn = mysql.connector.connect(
+    host= DB_HOST,
+    user= DB_USER,
+    password= DB_PASSWORD,
+    database= DB_NAME
+)
+cursor = conn.cursor()
+cursor.execute("""
+SELECT DATABASE()
+""")
+record = cursor.fetchone()
+print(record[0])
